2a545f210bd94466fce493ea77ce66cb3759c8ad
kambala3000 <kambala02@gmail.com>
2017-01-28 00:18:09 +0200

    guitarschool: our lessons, projects

--- guitarschool/app/js/main.js
+++ guitarschool/app/js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+$(document).ready(function() {
+    $('.proj-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        initialSlide: 1,
+        arrows: false,
+        dots: true,
+        fade: true,
+        speed: 800,
+        swipe: false
+    });
+});
