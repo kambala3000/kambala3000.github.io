16f95c69195cd669ed7754cae662f5b669c407b4
kambala3000 <kambala02@gmail.com>
2017-02-07 22:18:06 +0200

    calculator: started js

--- freecodecamp/js-calculator/app/js/main.js
+++ freecodecamp/js-calculator/app/js/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+var buttonsBox = document.getElementById('js-buttons-box'),
+    entryBox = document.getElementById('js-entry'),
+    historyBox = document.getElementById('js-history');
+
+var status = '';
+
+buttonsBox.onclick = function (e) {
+    var btn = e.target,
+        valBtn = btn.value;
+
+    if (valBtn == '/' || valBtn == '*' || valBtn == '-' || valBtn == '+') {
+        historyBox.innerHTML = entryBox.innerHTML;
+        entryBox.innerHTML = '0';
+    }
+
+    if (entryBox.innerHTML === '0' && valBtn != '.') {
+        entryBox.innerHTML = entryBox.innerHTML.slice(1);
+    }
+
+    console.log('kk2');
+    entryBox.innerHTML = entryBox.innerHTML + valBtn;
+};
