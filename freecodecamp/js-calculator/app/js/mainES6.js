ee97fae522b87cfef73c01e689fd3bfdb30f79f1
kambala3000 <kambala02@gmail.com>
2017-02-09 00:08:02 +0200

    calculator: js base

--- freecodecamp/js-calculator/app/js/mainES6.js
+++ freecodecamp/js-calculator/app/js/mainES6.js
@@ -4,21 +4,45 @@ var buttonsBox = document.getElementById('js-buttons-box'),
     entryBox = document.getElementById('js-entry'),
     historyBox = document.getElementById('js-history');
 
-var status = '';
+var status = '',
+    resultFlag = 0;
 
 buttonsBox.onclick = function (e) {
     var btn = e.target,
-        valBtn = btn.value;
+        valBtn = btn.value,
+        current = entryBox.innerHTML,
+        lastOperation = historyBox.innerHTML.slice(-1);
 
-    if (valBtn == '/' || valBtn == '*' || valBtn == '-' || valBtn == '+') {
-        historyBox.innerHTML = entryBox.innerHTML;
-        entryBox.innerHTML = '0';
+
+    if (valBtn === 'ce' || valBtn === 'ac') {
+        if (valBtn === 'ce') {
+            var cutLength = current.length;
+            historyBox.innerHTML = historyBox.innerHTML.slice(0, historyBox.innerHTML.length - cutLength);
+            entryBox.innerHTML = '0';
+        } else {
+            entryBox.innerHTML = '0';
+            historyBox.innerHTML = '';
+        }
+    } else {
+        if (valBtn === '/' || valBtn === '*' || valBtn === '-' || valBtn === '+') {  
+            if (current === '/' || current === '*' || current === '-' || current === '+') return;
+            if (lastOperation === '/' || lastOperation === '*' || lastOperation === '-' || lastOperation === '+') return;
+            entryBox.innerHTML = eval(historyBox.innerHTML);
+            resultFlag = 1;
+            //   entryBox.innerHTML = '0';
+        } else {
+            if (lastOperation === '/' || lastOperation === '*' || lastOperation === '-' || lastOperation === '+') {
+                entryBox.innerHTML = '0';
+            }
+            if (entryBox.innerHTML === '0' && valBtn !== '.') {
+                entryBox.innerHTML = entryBox.innerHTML.slice(1);
+            }
+            entryBox.innerHTML = entryBox.innerHTML + valBtn;
+        }
+        if (current === '/' || current === '*' || current === '-' || current === '+') {
+            entryBox.innerHTML = '0';
+        }
+        historyBox.innerHTML = historyBox.innerHTML + valBtn;
     }
-
-    if (entryBox.innerHTML === '0' && valBtn != '.') {
-        entryBox.innerHTML = entryBox.innerHTML.slice(1);
-    }
-
-    console.log('kk2');
-    entryBox.innerHTML = entryBox.innerHTML + valBtn;
+    // console.log('kk2');
 };
