afe922cbc3f56caa30ad44c525cc4ed541d4a2e4
kambala3000 <kambala02@gmail.com>
2017-02-16 00:11:44 +0200

    badnovels rework

--- badnovells/app/js/modal-window.js
+++ badnovells/app/js/modal-window.js
@@ -1,17 +1,24 @@
-$(document).ready(function() {
-	$('.modal-window').hide();
-})
-$('.gallery__item').click(function() {
-	console.log("log log");
-	var tubik = $(this).find('img[data-link="modal"]').attr('src');
-	console.log(tubik);
-	$('.modal-window img[data-link="received"]').attr('src', tubik);
-       console.log($('.modal-window img').attr("src"));
-       $('.modal-window').fadeIn({}, 50);
-})
-$('.close-modal').click(function(){
-	$('.modal-window').hide();
-})
-$('.cross-in-circle').click(function(){
-	$('.modal-window').hide();
-})
+(function () {
+	$('.gallery__item').click(function () {
+		if ($('.modal-window').length > 0) return;
+		var imgPath = $(this).find('img[data-link="modal"]').attr('src');
+		var format = imgPath.slice(imgPath.indexOf('.'));
+		imgPath = imgPath.slice(0, imgPath.length - format.length) + '-full' + format;
+		var modalContent = '<div class="modal-window js-close-modal"><div class="modal-frame"><div class="close-modal js-close-modal"></div><div class="cross-in-circle js-close-modal"></div><img src="' + imgPath + '"alt="screen" width="100%"></div>';
+		$('body').prepend(modalContent);
+		$('.modal-window').animate({
+			opacity: 1
+		}, 300);
+	});
+
+	$('body').on('click', '.js-close-modal', function (e) {
+		var targ = $(e.target);
+		if (targ.hasClass('js-close-modal')) {
+			$('.modal-window').animate({
+				opacity: 0
+			}, 300, function () {
+				this.remove()
+			});
+		}
+	});
+})();
