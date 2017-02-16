b680651de93f58c9d9f0373ac2b65947a9ddcd37
kambala3000 <kambala02@gmail.com>
2017-02-17 01:51:34 +0200

    added top-slider, reworked 2 and 3 section

--- badnovells/app/js/mainES6.js
+++ badnovells/app/js/mainES6.js
@@ -2,6 +2,20 @@
 
 (function () {
 
+    $(document).ready(function () {
+        $('.js-top-slider').slick({
+            autoplay: true,
+            autoplaySpeed: 3000,
+            initialSlide: 0,
+            arrows: false,
+            dots: false,
+            fade: true,
+            speed: 1800,
+            pauseOnHover: false,
+            swipe: false
+        });
+    });
+
     $('.js-up').click(function () {
         $(window).scrollTo(0);
     });
