7a72123dbc33a69db7399c435bd907d4eface125
kambala3000 <kambala02@gmail.com>
2017-07-03 17:58:43 +0300

    query DOM elements

--- freecodecamp/pomodoro-clock/app/js/main.js
+++ freecodecamp/pomodoro-clock/app/js/main.js
@@ -1,6 +1,17 @@
 "use strict";
 
-var inputs = document.getElementsByClassName("pomodoro__settings-input");
+var inputs = document.getElementsByClassName("pomodoro__settings-input"),
+    btnReduceBreak = document.getElementById("break-reduce"),
+    btnAddBreak = document.getElementById("break-add"),
+    inputBreakLength = document.getElementById("break-length"),
+    btnReduceSession = document.getElementById("session-reduce"),
+    btnAddSession = document.getElementById("session-add"),
+    inputSessionLength = document.getElementById("session-length"),
+    nodeTimerBlock = document.getElementById("timer-block"),
+    nodeTimerValue = document.getElementById("timer-value"),
+    nodeTimerFill = document.getElementById("timer-fill"),
+    btnStartTimer = document.getElementById("start-timer"),
+    btnResetTimer = document.getElementById("reset-timer");
 
 for (var i = 0; i < inputs.length; i++) {
     inputs[i].oninput = function () {
