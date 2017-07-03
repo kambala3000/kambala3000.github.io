90ea0217d207685715d747d68c3ddaf4b0c1db00
kambala3000 <kambala02@gmail.com>
2017-07-03 16:21:18 +0300

    styles, input valid

--- freecodecamp/pomodoro-clock/app/js/main.js
+++ freecodecamp/pomodoro-clock/app/js/main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var inputs = document.getElementsByClassName("pomodoro__settings-input");
+
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].oninput = function () {
+        this.value = this.value.replace(/[^0-9]/g, "");
+    };
+}
--- freecodecamp/pomodoro-clock/app/js/mainES6.js
+++ freecodecamp/pomodoro-clock/app/js/mainES6.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const inputs = document.getElementsByClassName("pomodoro__settings-input");
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].oninput = function() {
+        this.value = this.value.replace(/[^0-9]/g, "");
+    };
+}
